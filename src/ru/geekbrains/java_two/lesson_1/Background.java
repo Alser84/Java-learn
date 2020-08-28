9ca7ac1961dcf57fd73607cff712c524144533ec
Aleksey Kudryavtsev <alex13947@gmail.com>
2020-08-28 17:17:34 +0500

    Первый коммит

---
+++ src/ru/geekbrains/java_two/lesson_1/Background.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.java_two.lesson_1;
+
+import javax.swing.*;
+import java.awt.*;
+//меняем цвет канвы, без учёта времени
+public class Background extends JFrame {
+
+    public void paintBackround(){
+        setBackground(Color.red);
+        setVisible(true);
+    }
+
+}
